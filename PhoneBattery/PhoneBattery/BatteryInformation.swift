50b8b0b0dad162f658502a00ac654b710bec66c2
Marcel Voss <me@marcelvoss.com>
2017-02-21 22:44:16 +0100

    Time to start over

---
+++ PhoneBattery/PhoneBattery/BatteryInformation.swift
@@ -0,0 +1,13 @@
+//
+//  BatteryInformation.swift
+//  PhoneBattery
+//
+//  Created by Marcel Voß on 21/02/2017.
+//  Copyright © 2017 Marcel Voss. All rights reserved.
+//
+
+import UIKit
+
+class BatteryInformation: NSObject {
+
+}
---
+++ PhoneBattery/PhoneBattery/IAPManager.swift
@@ -0,0 +1,13 @@
+//
+//  IAPManager.swift
+//  PhoneBattery
+//
+//  Created by Marcel Voß on 21/02/2017.
+//  Copyright © 2017 Marcel Voss. All rights reserved.
+//
+
+import UIKit
+
+class IAPManager: NSObject {
+
+}
